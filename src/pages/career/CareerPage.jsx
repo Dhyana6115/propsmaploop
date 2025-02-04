54a5b60d09b20e008b6b8df0a71fd3984fdb470a
Dhyana6115 <dhyanarnw1997@gmail.com>
2025-02-04 12:20:33 +0530

    Update CareerPage.jsx

--- src/pages/career/CareerPage.jsx
+++ src/pages/career/CareerPage.jsx
@@ -5,13 +5,13 @@ import CareerCardUi from "./CareerCardUi";
 const CareerPage = () => {
   return (
     <div className="container py-5 ">
-      <h1 className="text-center mb-4">
+      <h1 className="text-center mb-5">
         Pick The Right Course To Build Your <br />
         Career
       </h1>
-      <div className="row g-3">
+      <div className="row  g-4 justify-content-center">
         {Career.map((item, index) => (
-          <div key={index} className="col-md-2 col-lg-2">
+          <div key={index} className="col-md-3 col-lg-2">
             <CareerCardUi title={item.title} icon={item.icon} />
           </div>
         ))}
