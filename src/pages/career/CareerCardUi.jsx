f0b6b5d6897f655f62310d93a64dee6192a6e393
Dhyana6115 <dhyanarnw1997@gmail.com>
2025-02-04 12:19:49 +0530

    Update CareerCardUi.jsx

--- src/pages/career/CareerCardUi.jsx
+++ src/pages/career/CareerCardUi.jsx
@@ -1,10 +1,15 @@
 const CareerCardUi = ({ title, icon }) => {
   return (
     <div
-      className="card shadow border-0 text-center p-3 "
-      style={{ height: "150px" }}
+      className="card shadow-lg border-0 text-center p-4"
+      style={{
+        height: "170px",
+        borderRadius: "10px",
+        
+        
+      }}
     >
-      <div className="mb-3" style={{ fontSize: "2rem" }}>
+      <div className="mx-3" style={{ fontSize: "2.5rem"}}>
         {icon}
       </div>
       <h5 className="card-title">{title}</h5>
