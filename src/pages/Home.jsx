989ed9c06ad898261c16daa87b24430d013c8575
Dhyana6115 <dhyanarnw1997@gmail.com>
2025-02-07 13:14:10 +0530

    Update Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,13 +1,15 @@
-// import CareerPage from "./page/CareerPage"
-// import LocationPage from "./LocationPage";
-// import MentorPage from "./MentorPage";
+import CareerPage from "./page/CareerPage"
+import LocationPage from "./LocationPage";
+import MentorPage from "./MentorPage";
+import  CoursePage from "./CoursePage";
 
 const Home = ()=>{
     return(
         <> 
-        {/* <CareerPage />
+        <CareerPage />
         <MentorPage />
-        <LocationPage /> */}
+        <LocationPage />
+        <CoursePage />
         </>
     )
 }
